9697f1ba9f6ddecbb48dde8bc265ba904d9ed360
Pasha <Pasha@MATEBOOK16S>
2023-10-11 17:41:00 +0300

    Добавьте файлы проекта.

---
+++ test3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.Design;
+
+namespace test3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = 5;
+            int z = 10;
+
+            if (x >= y && x <= z)
+            {
+                Console.WriteLine($"Число {x} gопадает в диапозон ");
+            }
+            else
+                if (x < y)
+            {
+                Console.WriteLine($"Число {x} не попадает в диапазон ");
+            }
+            else
+                if (x > z)
+            {
+                Console.WriteLine($"Число {x} не попадает в диапазон ");
+            }
+            else;
+        }
+    }
+}
